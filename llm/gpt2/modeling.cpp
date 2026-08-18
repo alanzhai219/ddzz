6aee06ba74a31726ba8ce5fbdd428f99d96c0e02
alanzhai219 <zhaixiuchuan@hotmail.com>
2026-08-18 14:55:43 +0800

    attn: impl 1

--- llm/gpt2/modeling.cpp
+++ llm/gpt2/modeling.cpp
@@ -28,11 +28,42 @@ void GPT2::transfomer_layer(size_t layer_id, Tensor& x, size_t n_past) {
 }
 
 void GPT2::attn(size_t layer_id, Tensor& x, size_t n_past) {
-    // TODO
+    const LayerWeights& lw = m_w.layers[layer_id];
+    
+    const auto shape = x.shape();   // [S, n_embd]
+    const size_t S = shape[0];
+    const size_t n_embd = shape[1];
+    const size_t n_head = m_w.config.n_head;
+    const size_t head_dim = m_w.hidden_dim;     // or called: head_dim
+    const size_t total = S + n_past;
+
+    // layer_norm
+    x = ops::layer_norm(x, lw.ln_1_w, lw.ln_1_b);   // [S, em_bd]
+    Tensor qkv = ops::matmul_2d(x, lw.attn_c_attn_w);        // [S, 3*em_bd]
+    qkv = ops::add(qkv, lw.attn_c_attn_b);              // [S, 3*em_bd]
+
+    // sdpa
+    // split qkv
+    Tensor q({S, n_embd});
+    Tensor k({S, n_embd});
+    Tensor v({S, n_embd});
+    ops::split_qkv(qkv, q, k, v);
+    
+    // kv-cache
+    std::vector<float>& kc = m_kv_cache.k(layer_id);
+    std::vector<float>& vc = m_kv_cache.k(layer_id);
+    kc.reserve(total * n_embd);
+    vc.reserve(total * n_embd);
+    kc.insert(kc.end(), k.ptr(), k_ptr() + k.size());
+    vc.insert(vc.end(), v.ptr(), v_ptr() + v.size());
+
+    // [S, e_embd] => [n_head, S, head_dim]
+    Tensor Q = split_head(q, S, n_head, head_dim);
+    // ...
 }
 
 void GPT2::ffn(size_t layer_id, Tensor& x, size_t n_past) {
-    // TODO
+    const LayerWeights& lw = m_w.layers[layer_id];
 }
 
 }
